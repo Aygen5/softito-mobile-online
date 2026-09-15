ac430aff85aadcb200cc3ef406ce27889690dc90
Aygen5 <aygenyildirim27@gmail.com>
2026-09-15 09:54:54 +0300

    feat: profile rol alani eklendi

--- odev-1/profil_servisi.dart
+++ odev-1/profil_servisi.dart
@@ -2,15 +2,18 @@
 class ProfilServisi {
   String kullaniciAdi;
   String durum;
+  String rol; // feature/profil-duzenle tarafindan eklendi
 
   ProfilServisi({
     required this.kullaniciAdi,
     this.durum = "Aktif",
+    this.rol = "Mobil Gelistirici",
   });
 
   void bilgileriGoster() {
     print("Kullanici: $kullaniciAdi");
     print("Durum: $durum");
+    print("Rol: $rol");
   }
 }
 
